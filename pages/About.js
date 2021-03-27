390117c2563504a7f3b2efba366825b3aba16618
Gear-500 <joshuanms@gmail.com>
2021-03-27 03:30:26 -0500

    about

--- pages/About.js
+++ pages/About.js
@@ -5,11 +5,15 @@ export default function About(props) {
 
     return (
         <View style={styles.container}> 
-            <Text style={styles.titleText}>Instructions</Text>
-            <Text>Rules: </Text>
-            <Text> 
-            Each player must pick each either rock, paper or scissors
-            </Text>
+            <Text style={styles.titleText}>About</Text>
+            <Text style={styles.headerText}>What is Rock Paper Scissors? </Text>
+            <Text style={styles.baseText}>A game based on luck and deception. It can have more than 2 players per game.</Text>
+            <Text style={styles.baseText}>Usually played out either by rounds or by elimination {"\n"}</Text>
+
+            <Text style={styles.headerText}>How To Play: </Text>
+            <Text style={styles.baseText}>Each player must pick between three items: rock, paper or scissors.</Text>
+            <Text style={styles.baseText}>If a player plays rock and the other plays paper, paper wins. And so on...</Text>
+            <Text style={styles.baseText}>The general rule is: rock beats scissors, scissors beat paper, and paper beats rock </Text>
         </View>
     )
 }
@@ -23,13 +27,12 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "red",
         padding: 25
-
     },
     titleText: {
         fontSize: 25,
         fontWeight: "bold",
         backgroundColor: "yellow",
-        padding: 10
+        padding: 15
     },
     headerText: {
         fontSize: 18,
@@ -38,6 +41,5 @@ const styles = StyleSheet.create({
     },
     baseText: {
         fontSize: 15,
-
     },
 });
